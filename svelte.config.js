cecd46e8093eaad62930b4cbe5f7c594f90111cd
proPianist1124 <phantomforces11242009@gmail.com>
2024-05-30 17:32:07 -0700

    socket.io fix #2

--- svelte.config.js
+++ svelte.config.js
@@ -7,13 +7,6 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		vite: {
-			resolve: {
-				alias: {
-					"xmlhttprequest-ssl": "./node_modules/engine.io-client/lib/xmlhttprequest.js",
-				},
-			},
-		},
 		alias: {
 			"@components/*": "./src/lib/components",
 		}
